e87554c52d2e3bb2ee91f5fe32a160dc9a2c3ec8
HARIHARAN <uidk6448@contiwan.com>
2021-02-02 22:44:28 +0530

    Initial Commit

---
+++ terraform/upload_bucket.tf
@@ -0,0 +1,19 @@
+variable "aws_s3_bucket_upload_name" {
+  default = "sns-sqs-upload-bucket"
+}
+
+resource "aws_s3_bucket" "upload" {
+  bucket = "${var.aws_s3_bucket_upload_name}"
+  acl    = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_notification" "upload" {
+  bucket = "${aws_s3_bucket.upload.id}"
+
+  topic {
+    topic_arn     = "${aws_sns_topic.upload.arn}"
+    events        = ["s3:ObjectCreated:*"]
+    filter_suffix = ".csv"
+  }
+}
